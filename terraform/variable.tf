c9bde31013014f7feb46ab04a3ee449c824c0eb8
Raghavarao1201 <raghavaraomachiraju@gmail.com>
2025-04-22 22:49:56 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -1,9 +1,3 @@
-variable "aws_region" {
-  type = string
-  description = "The AWS region to deploy resources in."
-  default = "us-east-1"
-}
-
 variable "cluster_name" {
   type = string
   description = "The name of the EKS cluster."
