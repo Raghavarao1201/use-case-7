4cd24ae6e4160581975b0da42648ab2486e00057
Raghavarao1201 <raghavaraomachiraju@gmail.com>
2025-05-16 04:43:53 +0000

    kubernetes ingress

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -74,3 +74,14 @@ resource "aws_route_table" "private_route_table" {
     nat_gateway_id = aws_nat_gateway.nat_gw.id
   }
 }
+
+# Associate Public Subnets with Public Route Table
+resource "aws_route_table_association" "private_route_association_1" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_route_table_association" "private_route_association_2" {
+  subnet_id      = aws_subnet.private_subnet_2.id
+  route_table_id = aws_route_table.private_route_table.id
+}
